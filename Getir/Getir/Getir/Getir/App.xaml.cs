684679cb6c465dd9c0a62fd260fa21069a574d20
1emrecakici <1emrecakici@gmail.com>
2021-05-22 00:17:16 +0300

    first commit

---
+++ Getir/Getir/Getir/Getir/App.xaml.cs
@@ -0,0 +1,36 @@
+﻿using Getir.Services;
+using Getir.Views;
+using System;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+using DLToolkit.Forms.Controls;
+using System.IO;
+
+namespace Getir
+{
+    public partial class App : Application
+    {
+        
+        public App()
+        {
+           
+            InitializeComponent();
+
+            DependencyService.Register<MockDataStore>();
+            MainPage = new AppShell();
+            FlowListView.Init();
+        }
+
+        protected override void OnStart()
+        {
+        }
+
+        protected override void OnSleep()
+        {
+        }
+
+        protected override void OnResume()
+        {
+        }
+    }
+}
